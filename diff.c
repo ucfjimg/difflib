c49f19f95e3b4b86e6fc00724c280e6711bb223c
Jim Geist <jimg@knights.ucf.edu>
2021-05-13 20:00:01 -0400

    checkpoint

--- diff.c
+++ diff.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct eclass_t eclass_t;
+#ifndef MIN
+# define MIN(a,b) ((a) < (b) ? (a) : (b))
+#endif
 
-struct eclass_t {
+typedef struct serhash_t serhash_t;
+struct serhash_t {
     unsigned serial;
     unsigned hash;
+};
+
+typedef struct eclass_t eclass_t;
+struct eclass_t {
+    unsigned serial;
     int last;
 };
 
 typedef struct vec_t vec_t;
-
 struct vec_t {
     size_t elesize;
     unsigned alloc;
     unsigned n;
     char *data;
+};
+
+typedef struct candidate_t candidate_t;
+struct candidate_t {
+    int a;
+    int b;
+    candidate_t *prev;
 };
 
 FILE *fp[2];
@@ -115,21 +129,40 @@ void vec_free(vec_t *v)
 }
 
 /*****************************************************************************
- * equivalence classes
+ * serial-hash vector
  */
+#define SH_INIT(sh) vec_init(sh, sizeof(serhash_t))
+#define SH_APPEND(sh) ((serhash_t*)vec_append(sh))
+#define SH_AT(sh, idx) \
+    ( ((serhash_t*)((sh)->data)) + (idx) )
 
+/*****************************************************************************
+ * equivalence class vector
+ */
 #define EV_INIT(ev) vec_init(ev, sizeof(eclass_t))
 #define EV_APPEND(ev) ((eclass_t*)vec_append(ev))
 #define EV_AT(ev, idx) \
     ( ((eclass_t*)((ev)->data)) + (idx) )
 
-/**
- * Compare two eq class nodes on hash, then serial
+/*****************************************************************************
+ * vector of integers
  */
-int ev_compare(const void *l, const void *r)
+#define INTV_INIT(iv) vec_init(iv, sizeof(int))
+#define INTV_APPEND(iv) ((int*)(vec_append(iv)))
+#define INTV_AT(iv, idx) \
+    ( ((int*)((iv)->data)) + (idx) )
+
+/*****************************************************************************
+ * serial-hash functions
+ */
+
+/**
+ * Compare two serial-hash nodes on hash, then serial
+ */
+int sh_compare(const void *l, const void *r)
 {
-    const eclass_t *ln = (const eclass_t *)l;
-    const eclass_t *rn = (const eclass_t *)r;
+    const serhash_t *ln = (const serhash_t *)l;
+    const serhash_t *rn = (const serhash_t *)r;
 
     if (ln->hash < rn->hash) {
         return -1;
@@ -150,41 +183,35 @@ int ev_compare(const void *l, const void *r)
     return 0;
 }
 
-/*****************************************************************************
- * vector of integers
- */
-#define INTV_INIT(iv) vec_init(iv, sizeof(int))
-#define INTV_APPEND(iv) ((int*)(vec_append(iv)))
-#define INTV_AT(iv, idx) \
-    ( ((int*)((iv)->data)) + (idx) )
-
 /**
- * Sort an eq class vector on hash, then serial
+ * Sort a serial-hash vector on hash, then serial
  */
-void ev_sort(vec_t *ev)
+void sh_sort(vec_t *sh)
 {
-    qsort(ev->data + sizeof(eclass_t), ev->n - 1, sizeof(eclass_t), &ev_compare);
+    // NB we skip the first placeholder node
+    qsort(sh->data + sizeof(serhash_t), sh->n - 1, sizeof(serhash_t), &sh_compare);
 }
 
 /**
  * Search for the last occurance of an equivalence class (based on hash)
- * in the given ec vector. Return the index or 0 if there is no match.
+ * in the given sh vector. Return the index or 0 if there is no match.
  */
-unsigned ev_search(vec_t *ev, unsigned hash)
+unsigned sh_search(serhash_t *V, eclass_t *E, int n, unsigned hash)
 {
     unsigned low = 1;
-    unsigned high = ev->n - 1;
+    unsigned high = n - 1;
 
     while (low <= high) {
         unsigned mid = (low + high) / 2;
-        unsigned mid_h = EV_AT(ev, mid)->hash;
+        unsigned mid_h = V[mid].hash;
 
         if (hash < mid_h) {
             high = mid - 1;
         } else if (hash > mid_h) {
             low = mid + 1;
         } else {
-            while (!EV_AT(ev, mid-1)->last) {
+            printf("mid %d\n", mid);
+            while (!E[mid-1].last) {
                 mid--;
             }
 
@@ -195,96 +222,165 @@ unsigned ev_search(vec_t *ev, unsigned hash)
     return 0;
 }
 
-/**
- * Parse an input file into (serial, hash) tuples, where each tuple
- * represents a line and 'serial' is the 1-based line number.
+/*****************************************************************************
+ * candidates
  */
-void parse_right_file(FILE *fp, vec_t *ev)
+candidate_t* candidate(int a, int b, candidate_t *prev)
 {
+    candidate_t *p = (candidate_t *)safe_malloc(sizeof(candidate_t));
+    p->a = a;
+    p->b = b;
+    p->prev = prev;
+    return p;
+}
+
+/*****************************************************************************
+ * file parsing and algorithm
+ */
+
+/**
+ * Read the next line from 'fp' and compute its hash.
+ * 
+ * Return 1 on success with 'phash' containing the hash.
+ * Return 0 on EOF w/ no data to hash. 
+ */
+int next_line(FILE *fp, unsigned *phash)
+{
+    int ch;
     const unsigned HASH0 = 5381;
     unsigned hash = HASH0;
-    int ch;
-    eclass_t *ec;
 
-    EV_INIT(ev);
-    ec = EV_APPEND(ev);
-    ec->hash = ~1;
-    ec->serial = 0;
-    ec->last = 0;
+    if (feof(fp)) {
+        return 0;
+    }
 
     while ((ch = fgetc(fp)) != EOF) {
-        if (ch != '\r' && ch != '\n') {
-            hash = ((hash << 5) + hash) + ch;
-            continue;
+        if (ch == '\r' || ch == '\n') {
+            break;
         }
+        hash = ((hash << 5) + hash) + ch;
+    }
 
-        ec = EV_APPEND(ev);
-        ec->serial = ev->n - 1;
-        ec->hash = hash;
-        ec->last = 0;
+    // we read a blank link that ended in EOF - that counts as EOF
+    //
+    if (ch == EOF && hash == HASH0) {
+        return 0;
+    }
 
-        hash = HASH0;
-
-        if (ch == '\r') {
-            ch = fgetc(fp);
-            if (ch != '\n') {
-                ungetc(ch, fp);
-            }
+    if (ch == '\r') {
+        ch = fgetc(fp);
+        if (ch != '\n') {
+            ungetc(ch, fp);
         }
+    }
+
+    *phash = hash;
+    return 1;
+}
+
+/**
+ * Parse the right-hand file into (serial, hash) pairs where serial is 
+ * the one-based line number. 
+ */
+void parse_right_file(FILE *fp, vec_t *V)
+{
+    unsigned hash;
+    serhash_t *sh;
+
+    SH_INIT(V);
+    sh = SH_APPEND(V);
+    sh->hash = ~1;
+    sh->serial = 0;
+
+    while (next_line(fp, &hash)) {
+        sh = SH_APPEND(V);
+        sh->serial = V->n - 1;
+        sh->hash = hash;
     }
 }
 
 /**
  * Build the equivalence classes for a vector of (serial, hash) pairs.
- * Sorts the vector in-place.
  */
-void buildeq(vec_t *ev)
+void buildeq(vec_t *V, vec_t *E)
 {
     int i;
+    serhash_t *sh;
     eclass_t *ec;
 
-    ev_sort(ev);
-    ec = EV_AT(ev, 0);
+    EV_INIT(E);
+    ec = EV_APPEND(E);
+    ec->serial = 0;
+    ec->last = 1;
+    
+    sh_sort(V);
+    ec = EV_AT(E, 0);
     ec->last = 1;
 
-    for (i = 1; i < ev->n; i++) {
-        ec = EV_AT(ev, i);
-        ec->last = (i == ev->n - 1) || (ec->hash != ec[1].hash); 
+    for (i = 1; i < V->n; i++) {
+        sh = SH_AT(V, i);
+        ec = EV_APPEND(E);
+        ec->serial = sh->serial;
+        ec->last = (i == V->n - 1) || (sh->hash != sh[1].hash); 
     }
 }
 
 /**
- *
+ * Parse the left hand file in terms of the right hand file's
+ * eq classes.
  */
-void parse_left_file(FILE *fp, vec_t *ev, vec_t *iv)
+void parse_left_file(FILE *fp, vec_t *V, vec_t *E, vec_t *P)
 {
-    const unsigned HASH0 = 5381;
-    unsigned hash = HASH0;
-    int ch;
-    int i, j;
+    unsigned hash;
+    int i;
 
-    INTV_INIT(iv);
-    *INTV_APPEND(iv) = 0;
+    INTV_INIT(P);
+    *INTV_APPEND(P) = 0;
 
-    j = 1;
-    while ((ch = fgetc(fp)) != EOF) {
-        if (ch != '\r' && ch != '\n') {
-            hash = ((hash << 5) + hash) + ch;
-            continue;
-        }
+    while (next_line(fp, &hash)) {
+        i = sh_search(SH_AT(V, 0), EV_AT(E, 0), V->n, hash);
+        *INTV_APPEND(P) = i;
+    }
+}
 
-        i = ev_search(ev, hash);
-        *INTV_APPEND(iv) = i;
+void merge(candidate_t **K, int *k, int i, vec_t *E, int p)
+{
+    int r = 0;
+    candidate_t *c = K[0];
+    int s;
 
-        hash = HASH0;
+    for(;;) {
+        int j = EV_AT(E, p)->serial;
 
-        if (ch == '\r') {
-            ch = fgetc(fp);
-            if (ch != '\n') {
-                ungetc(ch, fp);
+        // TODO bsearch
+        for (s = r; s <= *k; s++) {
+            if (K[s]->b < j && K[s+1]->b > j) {
+                break;
             }
         }
+
+        if (s <= *k) {
+            if (K[s+1]->b > j) {
+                K[r] = c;
+                r = s + 1;
+                c = candidate(i, j, K[s]);
+            }
+
+            if (s == *k) {
+                K[*k+2] = K[*k+1];
+                ++*k;
+                break;
+            }
+        }
+
+        if (EV_AT(E, p)->last) {
+            break;
+        }
+
+        p++;
     }
+
+    K[r] = c;
 }
 
 void usage(void)
@@ -296,9 +392,14 @@ int main(int argc, char **argv)
 {
     int i;
     vec_t V;
+    vec_t E;
     vec_t P;
+    int m, n;
+    int k_size, k;
+    candidate_t **K;
+    int *pp;
 
-    if (argc != 3) {
+    if (argc != 3) { 
         usage();
         return 1;
     }
@@ -313,6 +414,53 @@ int main(int argc, char **argv)
     } 
 
     parse_right_file(fp[1], &V);
-    buildeq(&V);
-    parse_left_file(fp[0], &V, &P);
+
+    printf("V hash serial:\n");
+    for (i = 0; i < V.n; i++) {
+        serhash_t *sh = SH_AT(&V, i);
+        printf(" %u %u\n", sh->hash, sh->serial);
+    }
+
+    sh_sort(&V);
+    buildeq(&V, &E);
+
+    printf("E serial last:\n");
+    for (i = 0; i < E.n; i++) {
+        eclass_t *ec = EV_AT(&E, i);
+        printf(" %u %u\n", ec->serial, ec->last);
+    }
+
+    parse_left_file(fp[0], &V, &E, &P);
+
+    printf("P index into E:\n");
+    for (i = 0; i < P.n; i++) {
+        printf(" %u\n", *INTV_AT(&P, i));
+    }
+
+    vec_free(&V);
+
+    n = E.n;
+    m = P.n;
+
+    k_size = MIN(m, n) + 2;
+    K = (candidate_t **)safe_malloc(k_size * sizeof(candidate_t *));
+    k = 0;
+    K[0] = candidate(0, 0, NULL);
+    K[1] = candidate(m + 1, n + 1, NULL);
+
+    pp = INTV_AT(&P, 0);
+    for (i = 1; i <= m; i++) {
+        if (pp[i]) {
+            merge(K, &k, i, &E, pp[i]);
+        }
+    }
+
+    for (i = 0; i <= k; i++) {
+        candidate_t *c;
+        printf("i = %u\n", i);
+
+        for (c = K[i]; c; c = c->prev) {
+            printf("  (%u,%u)\n", c->a, c->b);
+        }
+    }
 }
